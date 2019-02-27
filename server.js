c1347a5efc34bc43184c4ceebaac3e4a7888d4df
Daniel K <34487179+dmdddd@users.noreply.github.com>
2019-02-27 16:58:16 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded({extended: true})); // to support URL-encoded bodies
 
 app.listen(port, () => {
-	console.log('Garage Inc app listening on port %d!', port);
+	console.log('Hostel Manager is listening on port %d!', port);
 });
 
 //=============================================
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/main.html');
 });
 
-// New job adding page
+// Edit Layout
 app.get('/edit', (req, res) => {
 	res.sendFile(__dirname + '/edit.html');
 });
@@ -34,49 +34,23 @@ app.get('/edit', (req, res) => {
 //=============================================
 // User requests
 //=============================================
-app.post('/Login', function(req, res){
-  var username = req.body.name;
-  var password = req.body.password;
-  var found = {'success': false, 'message': 'Username or password are wrong'};
-  var usersRef = admin.firestore().collection('Users').doc(username);
-  var serDoc = usersRef.get()
-    .then(doc => {
-      if (doc.exists && doc.data().password == password){
-        found.success = true;
-        found.message = "/Main";
-        found.name=doc.data().name;
-        res.send(found);
-      } else {
-        res.send(found);
-      }
-    })
-    .catch(err => {
-      console.log('Error getting document', err);
-    });
-});
-
-app.post('/Register', function(req, res){
-  var data = req.body;
-  var register_new_user = false;
-  var username = req.body.username;
-  var found = {'success': false, 'message': 'Could not register a user'};
-  var usersRef = admin.firestore().collection('Users').doc(username);
-  var serDoc = usersRef.get()
-    .then(doc => {
-      if (!doc.exists) {
-        // here we start regestering the user
-        register_new_user = true;
-        var setDoc = admin.firestore().collection('Users').doc(username).set(data);
-        found.success  = true;
-        found.message = "/Main";
-        res.send(found);
-
-      } else {
-        found.message = "User name is taken";
-        res.send(found);
-      }
-    })
-    .catch(err => {
-      console.log('Error getting document', err);
-    });
-});
+// app.post('/Login', function(req, res){
+  // var username = req.body.name;
+  // var password = req.body.password;
+  // var found = {'success': false, 'message': 'Username or password are wrong'};
+  // var usersRef = admin.firestore().collection('Users').doc(username);
+  // var serDoc = usersRef.get()
+    // .then(doc => {
+      // if (doc.exists && doc.data().password == password){
+        // found.success = true;
+        // found.message = "/Main";
+        // found.name=doc.data().name;
+        // res.send(found);
+      // } else {
+        // res.send(found);
+      // }
+    // })
+    // .catch(err => {
+      // console.log('Error getting document', err);
+    // });
+// });

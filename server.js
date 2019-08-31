432982a2bf27e39aa24b3932e733c8256c89df1a
Daniel K <34487179+dmdddd@users.noreply.github.com>
2019-08-31 09:09:59 +0300

    Updating to the latest version

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ app.listen(port, () => {
 //=============================================
 // Database
 //=============================================
-var floors = []
+//var default_layout = [{"name":"Floor 1","id":"floor1","rooms":[{"id":"room1","name":"Room 1","top":0,"left":40,"width":200,"height":60},{"id":"room2","name":"Room 2","top":60,"left":40,"width":60,"height":60},{"id":"room3","name":"Room 3","top":120,"left":40,"width":200,"height":60}]},{"name":"Floor 2","id":"floor2","rooms":[{"id":"room4","name":"Room 4","top":0,"left":40,"width":200,"height":60},{"id":"room5","name":"Room 5","top":60,"left":180,"width":60,"height":60},{"id":"room6","name":"Room 6","top":120,"left":40,"width":200,"height":60}]}];
+var default_layout = [{"name":"123","id":"123","height":200,"width":1540,"rooms":[]},{"name":"123321","id":"123321","height":200,"width":1540,"rooms":[]}];
+var user_saved_layout;
 
 //=============================================
 // HTML and css Pages
@@ -29,6 +31,35 @@ app.get('/', (req, res) => {
 // Edit Layout
 app.get('/edit', (req, res) => {
 	res.sendFile(__dirname + '/edit.html');
+});
+
+
+//=============================================
+// Data Handling requests
+//=============================================
+app.get('/default_layout', (req, res) => {
+  console.log(default_layout);
+	res.send(default_layout);
+});
+
+app.get('/user_saved_layout', (req, res) => {
+  console.log(user_saved_layout);
+	res.send(user_saved_layout);
+});
+
+app.post('/user_saved_layout', (req, res) => {
+  var result = {'success': true, 'message': 'Layout has been updated.'};
+  var object = req.body;
+  user_saved_layout = object;
+
+
+  console.log("DEF");
+  console.log(typeof default_layout);
+  console.log("USERS");
+  console.log(typeof user_saved_layout);
+  console.log(object);
+
+  res.send(result);
 });
 
 //=============================================
